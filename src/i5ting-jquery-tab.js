efacb14797ccb6dd0320c1aa353e52d336dfdcbc
shiren1118 <shiren1118@126.com>
2014-01-29 09:36:22 +0800

    refact

--- src/i5ting-jquery-tab.js
+++ src/i5ting-jquery-tab.js
@@ -7,7 +7,10 @@
  */
 
 (function($) {
-
+	
+	/**
+	 * private: 给li绑定hover效果
+	 */
 	function _tab_list_li_hover(){
 	    //tab 头hover时间处理 
 	  	$(".i5ting_tab_list li").hover(function(){
@@ -17,23 +20,17 @@
 	  	});		
 	}
 	
-	function _init_tab_content_btn(container){
-		$(container).find('.i5ting_tab_list #up_sen').click(function(){
-			if($(this).hasClass('up')){
-				$(this).removeClass('up').addClass('down');
-				$(container).find('.i5ting_tab_content').hide();
-			}else{
-				$(this).removeClass('down').addClass('up');
-				$(container).find('.i5ting_tab_content').show();
-			}
-		});
-	}
-	
+	/**
+	 * private: 初始化tab content
+	 */
 	function _init_tab_content(container,opts){
 		$(container).find('.i5ting_tab_content div.i5ting_tab_content_item')
 			.eq( opts.current_tab_index ).show().siblings().hide();		
 	}
 	
+	/**
+	 * private: 如果is_tab_content_btn_show=yes，则显示上下箭头
+	 */
 	function _add_tab_content_btn_if_need(container,opts){
 		if(opts.is_tab_content_btn_show){
 			$('<a></a>',
@@ -44,22 +41,45 @@
 				}
 			).prependTo( $(container).find('.i5ting_tab_list') );
 			
-			console.log('already show tab control button'+ $(container));
+			$(container).find('.i5ting_tab_list #up_sen').click(function(){
+				if($(this).hasClass('up')){
+					$(this).removeClass('up').addClass('down');
+					$(container).find('.i5ting_tab_content').hide();
+				}else{
+					$(this).removeClass('down').addClass('up');
+					$(container).find('.i5ting_tab_content').show();
+				}
+			});
 		}
-		
-		_init_tab_content_btn( $(container) );
 	}
 	
+	/**
+	 * private: 给tab header的最后一个里增加样式，fixed
+	 */
 	function _add_fix_height_if_need(container,opts){
 		if(opts.fix_height){
 			$(container).find('.i5ting_tab_content_item').css('height',opts.fix_height);	
 		}
 	}
 	
+	/**
+	 * private: 
+	 */
 	function _add_class_for_last_li(container,opts){
 		$(container).find('.i5ting_tab_list li').last().addClass('last');
 	}
 	
+	/**
+	 * private: 应用配置，完成对应效果
+	 */
+	function _apply_render_with_config(container,opts){
+		// 如果配置项里有opts.fix_height
+		_add_fix_height_if_need($(container),opts);
+	}
+	
+	/**
+	 * public: 事件处理
+	 */
 	function event_process(container,opts){
 		switch(opts.event_trigger_type){
 			case 'hover':
@@ -89,20 +109,29 @@
 		 }
 	}
 	
+	/**
+	 * public: 初始化tab ui界面
+	 */
 	function init_tab_ui(container,opts){
+		// 给li绑定hover效果
 		_tab_list_li_hover();	
 		
-		//如果is_tab_content_btn_show=yes，则显示上下箭头
+		// 如果is_tab_content_btn_show=yes，则显示上下箭头
 		_add_tab_content_btn_if_need($(container),opts);
 		
+		// 给tab header的最后一个里增加样式，fixed
 		_add_class_for_last_li($(container),opts);
 		
+		// 初始化tab content
 		_init_tab_content($(container),opts);
 
-		// 如果配置项里有opts.fix_height
-		_add_fix_height_if_need($(container),opts);
+		// 应用配置，完成对应效果
+		_apply_render_with_config($(container),opts);
 	}
 	
+	/**
+	 * 插件
+	 */
   	$.fn.i5ting_jquery_tab = function(options) {
 	  	var opts = $.extend({}, $.fn.i5ting_jquery_tab.defaults, options);  
 	
@@ -113,6 +142,9 @@
 		
   	};//end i5ting_jquery_tab
 	
+	/**
+	 * 插件默认项
+	 */
   	$.fn.i5ting_jquery_tab.defaults = {   
 		current_tab_index: 0, //从0开始
 		is_tab_content_btn_show: true, /*显示上下箭头*/
